829d16df84e61842d8742a8d296390dcba970683
DESKTOP-R1GDCIE\이소연 <christian404@naver.com>
2026-01-07 14:24:49 +0900

    기본 gameMode, gameInstance, playerController 클래스 추가

---
+++ ProjectD/Source/ProjectD/PDGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTD_API APDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
