9b110e02ba16152bec183244b646e2684f5275af
llssyy404 <christian404@naver.com>
2026-01-19 16:55:08 +0900

    캐릭터 생성,삭제 관리하는 ModelManager 추가

--- ProjectD/Source/ProjectD/Battle/PDBattleGameMode.cpp
+++ ProjectD/Source/ProjectD/Battle/PDBattleGameMode.cpp
@@ -2,22 +2,19 @@
 
 
 #include "PDBattleGameMode.h"
+
+#include "../Manager/ModelManager.h"
 #include "../Character/PDCharacter.h"
+
 
 void APDBattleGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString Path = TEXT("/Game/Unit/Blueprints/BP_Nana.BP_Nana_C");
-	UClass* ObjectClass = StaticLoadClass(UObject::StaticClass(), NULL, *Path, NULL, LOAD_None, NULL);
-	if (ObjectClass == nullptr)
-	{
-		return;
-	}
-
 	FVector Pos(4979.f, 5132.f, -193.f);
 	FRotator Rot(0.0f, 20.0f, 0.0f);
-	FActorSpawnParameters blueSpawnInfo;
-	blueSpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	APDCharacter* PDCharacter = GetWorld()->SpawnActor<APDCharacter>(ObjectClass, Pos, Rot, blueSpawnInfo);
+	if (ModelManager)
+	{
+		ModelManager->SpawnCharacter(1, Pos, Rot);
+	}
 }
